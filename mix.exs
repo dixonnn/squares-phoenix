b29a7145a01cf2087e3adbc86ec3af24512754d9
Chris Dixon <chrisdixon3295@gmail.com>
2019-02-05 09:16:44 -0500

    version changes mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SquaresPhoenix.Mixfile do
     [
       app: :squares_phoenix,
       version: "0.0.1",
-      elixir: "~> 1.7",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
